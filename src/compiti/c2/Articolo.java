c7a6832ad11d4551b89ea66bcf2f3fb55f114af9
Alina Petrova <60265738+Alina-Petrova@users.noreply.github.com>
2021-01-31 11:23:17 +0100

    modifica

---
+++ src/compiti/c2/Articolo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compiti.c2;
+
+import compiti.c1.*;
+
+/**
+ *
+ * @author utente
+ */
+public interface Articolo {
+
+    
+    
+    String getNome();
+    
+    double getPrezzo();
+
+    String toString();
+   
+    
+}
---
+++ src/compiti/c2/Catalogo.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compiti.c2;
+
+import compiti.c1.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author utente
+ */
+public class Catalogo <E extends Articolo> implements Iterable<E> {
+    
+    private final List<E> catalogoArt;
+    
+
+    public <E extends Articolo> Catalogo() {
+        catalogoArt = new ArrayList<>();
+    }
+    
+    public void addArticolo(E art) {
+        catalogoArt.add(art);
+    }
+    
+    public Catalogo<E> getArticoliSottoPrezzo(double prezzo) {
+        Catalogo<E> ris = new Catalogo<>();
+        for(E el: catalogoArt) {
+            if(el.getPrezzo()<prezzo)
+                ris.addArticolo(el);
+        }
+        return ris;
+    }
+    
+    
+     @Override
+    public String toString() {
+        String ris = "";
+        for(E el: catalogoArt)
+            ris+=el.toString();
+        return ris;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new CatIterator();
+    }
+
+    private class CatIterator implements Iterator<E> {
+        private int idx;
+        public CatIterator() {
+            idx = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return idx<catalogoArt.size()-1;
+        }
+
+        @Override
+        public E next() {
+            return catalogoArt.get(idx++);
+        }
+    }
+
+   
+    
+}
